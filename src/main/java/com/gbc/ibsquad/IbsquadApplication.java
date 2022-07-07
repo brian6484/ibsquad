c7d3a592517ae2cd33f4a9c7cbd19e22e3e9d598
Brian <brianlee6484@gmail.com>
2022-07-07 16:13:34 +0900

    post code

--- src/main/java/com/gbc/ibsquad/IbsquadApplication.java
+++ src/main/java/com/gbc/ibsquad/IbsquadApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableJpaAuditing
-@SpringBootApplication
+//@SpringBootApplication
 public class IbsquadApplication {
 
 	public static void main(String[] args) {
