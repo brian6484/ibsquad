521c11bd9e2f95b74ad7d7b77dcc2d9b27fe7c71
Brian <brianlee6484@gmail.com>
2022-07-19 16:38:38 +0900

    minor thymeleaf changes

--- src/main/java/com/gbc/ibsquad/web/HomeController.java
+++ src/main/java/com/gbc/ibsquad/web/HomeController.java
@@ -2,8 +2,11 @@ package com.gbc.ibsquad.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Controller
