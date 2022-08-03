b0070a6bc088d5e784679f12b356ae07c511b15c
Brian <brianlee6484@gmail.com>
2022-08-03 16:35:51 +0900

    prehandle

--- src/main/java/com/gbc/ibsquad/web/interceptor/LogInterceptor.java
+++ src/main/java/com/gbc/ibsquad/web/interceptor/LogInterceptor.java
@@ -1,17 +1,28 @@
 package com.gbc.ibsquad.web.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
+@Slf4j
 public class LogInterceptor implements HandlerInterceptor {
     public static final String LOG_ID = "logId";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        request.setAttribute(LOG_ID, uuid);
+
+        if(handler instanceof HandlerMethod){
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+        return true;
     }
 
     @Override
