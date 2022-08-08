e58edeb35a06eca034c222afa94a85faf4f48756
Brian <brianlee6484@gmail.com>
2022-08-08 16:21:25 +0900

    interceptors

--- src/main/java/com/gbc/ibsquad/web/interceptor/LogInterceptor.java
+++ src/main/java/com/gbc/ibsquad/web/interceptor/LogInterceptor.java
@@ -27,11 +27,11 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+//        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
